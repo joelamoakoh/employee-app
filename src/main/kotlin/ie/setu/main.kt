c6bf6387962f00248768412ce0864e20b1c90043
joelamoakoh <20096482@mail.wit.ie>
2023-02-11 19:57:33 +0000

    Adding menu

--- src/main/kotlin/ie/setu/main.kt
+++ src/main/kotlin/ie/setu/main.kt
@@ -1,4 +1,7 @@
 package ie.setu
+
+import kotlin.math.round
+
 val firstName = "Joe"
 val surname = "Soap"
 val gender = 'm'
@@ -8,65 +11,76 @@ val payePercentage = 38.5
 val prsiPercentage = 5.2
 val annualBonus = 1450.50
 val cycleToWorkMonthlyDeduction = 54.33
+
 fun main(args: Array<String>){
-    println("Payslip Printer")
-    printPayslip()
-    println("Rounded Payslip")
-    printPayslipWithRounding()
 
+    var input : Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> println("Monthly Salary: ${getMonthlySalary()}")
+            2 -> println("Monthly PRSI: ${getMonthlyPRSI()}")
+            3 ->println("Monthly PAYE: ${getMonthlyPAYE()}")
+            4 -> println("Monthly Gross Pay: ${getGrossMonthlyPay()}")
+            5 -> println("Monthly Total Deductions: ${getTotalMonthlyDeductions()}")
+            6 -> println("Monthly Net Pay: ${getNetMonthlyPay()}")
+            7 -> println(getPayslip())
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
 }
 
-fun printPayslip(){
-
-    val monthlySalary = (grossSalary/12)
-    val monthlyPrsi = monthlySalary * (prsiPercentage / 100)
-    val monthlyPaye = monthlySalary * (payePercentage / 100)
-    val grossPay = (monthlySalary + (annualBonus/12))
-    val totalDeductions = (monthlyPrsi + monthlyPrsi + cycleToWorkMonthlyDeduction)
-
-    println ("_________________________________________________________________________")
-    println ("|                          Monthly Payslip                              |")
-    println ("|_______________________________________________________________________|")
-    println ("|                                                                       |")
-    println ("|   Employee Name:  " + firstName.uppercase() + " " + surname.uppercase() + "(" + gender.uppercase() + ")                    Employee ID: " + employeeID + " |")
-    println ("|                                                                       |")
-    println ("|_______________________________________________________________________|")
-    println ("|                                                                       |")
-    println ("|    PAYMENT DETAILS \t\t\t\t DEDUCTION DETAILS  \t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|    Salary: " + monthlySalary + "\t\t PAYE: " + monthlyPaye + "\t\t\t|")
-    println ("|    Bonus:  " + (annualBonus / 12) + "\t\t\t\t PRSI: " + monthlyPrsi  + "\t\t\t|")
-    println ("|    \t\t\t\t\t\t\t\t Cycle To Work: " + cycleToWorkMonthlyDeduction  + "\t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|    Gross:  " + grossPay + " \t\tTotal Deductions: " + totalDeductions + " |")
-    println ("|_______________________________________________________________________|")
-    println ("|   \t\t\t\t NET PAY:" + (grossPay - totalDeductions) + " \t\t\t\t\t\t\t|")
-    println ("|_______________________________________________________________________|")
+fun menu() : Int {
+    print("""
+         Employee Menu for ${getFullName()}
+           1. Monthly Salary
+           2. Monthly PRSI
+           3. Monthly PAYE
+           4. Monthly Gross Pay
+           5. Monthly Total Deductions
+           6. Monthly Net Pay
+           7. Full Payslip
+          -1. Exit
+         Enter Option : """)
+    return readLine()!!.toInt()
 }
-fun printPayslipWithRounding(){
 
-    val monthlySalary = (grossSalary/12)
-    val monthlyPrsi = monthlySalary * (prsiPercentage / 100)
-    val monthlyPaye = monthlySalary * (payePercentage / 100)
-    val grossPay = (monthlySalary + (annualBonus/12))
-    val totalDeductions = (monthlyPrsi + monthlyPrsi + cycleToWorkMonthlyDeduction)
-
-    println ("_________________________________________________________________________")
-    println ("|                          Monthly Payslip                              |")
-    println ("|_______________________________________________________________________|")
-    println ("|                                                                       |")
-    println ("|   Employee Name:  " + firstName.uppercase() + " " + surname.uppercase() + "(" + gender.uppercase() + ")                    Employee ID: " + employeeID + "    |")
-    println ("|                                                                       |")
-    println ("|_______________________________________________________________________|")
-    println ("|                                                                       |")
-    println ("|    PAYMENT DETAILS \t\t\t\t DEDUCTION DETAILS  \t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|    Salary: " + "%.2f".format(monthlySalary) + "\t\t\t\t PAYE: " + "%.2f".format(monthlyPaye) + "\t\t\t\t\t\t|")
-    println ("|    Bonus:  " + "%.2f".format((annualBonus / 12)) + "\t\t\t\t\t PRSI: " + "%.2f".format(monthlyPrsi)  + "\t\t\t\t\t\t|")
-    println ("|    \t\t\t\t\t\t\t\t Cycle To Work: " + cycleToWorkMonthlyDeduction  + "\t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|    Gross:  " + "%.2f".format(grossPay) + " \t\t\t\tTotal Deductions: " + "%.2f".format(totalDeductions) + " \t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|   \t\t\t\t\t\t NET PAY:" + "%.2f".format((grossPay - totalDeductions)) + " \t\t\t\t\t\t\t|")
-    println ("|_______________________________________________________________________|")
+fun getFullName() = when (gender){
+    'm', 'M' -> "Mr. $firstName $surname"
+    'f', 'F' -> "Ms. $firstName $surname"
+    else -> "$firstName $surname"
 }
+
+fun getMonthlySalary() = roundTwoDecimals(grossSalary / 12)
+fun getMonthlyPRSI() = roundTwoDecimals(getMonthlySalary() * (prsiPercentage / 100))
+fun getMonthlyPAYE() = roundTwoDecimals(getMonthlySalary() * (payePercentage / 100))
+fun getGrossMonthlyPay() = roundTwoDecimals(getMonthlySalary() + (annualBonus / 12))
+fun getTotalMonthlyDeductions() = roundTwoDecimals((getMonthlyPRSI() + getMonthlyPAYE() + cycleToWorkMonthlyDeduction))
+fun getNetMonthlyPay() = roundTwoDecimals(roundTwoDecimals(getGrossMonthlyPay() - getTotalMonthlyDeductions()))
+
+fun getPayslip() =
+    """
+        ______________________________________________________________________
+         Monthly Payslip:             ${getFullName()}, ID: $employeeID                  
+        ______________________________________________________________________    
+              PAYMENT DETAILS (gross pay: ${getGrossMonthlyPay()})                                                                    
+        ______________________________________________________________________
+                   Salary: ${getMonthlySalary()}
+                   Bonus:  ${roundTwoDecimals(annualBonus / 12)}            
+        ______________________________________________________________________
+              DEDUCTION DETAILS (total Deductions: ${getTotalMonthlyDeductions()})      
+        ______________________________________________________________________
+                   PAYE: ${getMonthlyPAYE()}                
+                   PRSI: ${getMonthlyPRSI()}  
+                   Cycle To Work: $cycleToWorkMonthlyDeduction         
+        ______________________________________________________________________
+             NET PAY: ${getNetMonthlyPay()} 
+        ______________________________________________________________________"""
+
+//https://discuss.kotlinlang.org/t/how-do-you-round-a-number-to-n-decimal-places/8843
+//There are several options...try each of them out
+fun roundTwoDecimals(number: Double) = round(number * 100) / 100
+//fun roundTwoDecimals(number: Double) = "%.2f".format(number).toDouble()
