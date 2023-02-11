e221d1fb3bf3c293e8ed1fe3b9ca979c830e9942
joelamoakoh <20096482@mail.wit.ie>
2023-02-11 20:25:48 +0000

    Allowing the user to add an Employee

--- src/main/kotlin/ie/setu/main.kt
+++ src/main/kotlin/ie/setu/main.kt
@@ -10,6 +10,7 @@ var Employee =  employee("Joe", "Soap", 'm', 6143, 67543.21, 38.5, 5.2, 1450.50,
 
 fun main(args: Array<String>){
 
+    add()
     var input : Int
 
     do {
@@ -79,3 +80,25 @@ fun getPayslip() =
 
 
 fun roundTwoDecimals(number: Double) = round(number * 100) / 100
+fun add(){
+    print("Enter first name: ")
+    val firstName = readLine().toString()
+    print("Enter surname: ")
+    val surname = readLine().toString()
+    print("Enter gender (m/f): ")
+    val gender = readLine()!!.toCharArray()[0]
+    print("Enter employee ID: ")
+    val employeeID = readLine()!!.toInt()
+    print("Enter gross salary: ")
+    val grossSalary = readLine()!!.toDouble()
+    print("Enter PAYE %: ")
+    val payePercentage = readLine()!!.toDouble()
+    print("Enter PRSI %: ")
+    val prsiPercentage = readLine()!!.toDouble()
+    print("Enter Annual Bonus: ")
+    val annualBonus= readLine()!!.toDouble()
+    print("Enter Cycle to Work Deduction: ")
+    val cycleToWorkMonthlyDeduction= readLine()!!.toDouble()
+
+    Employee = employee(firstName, surname, gender, employeeID, grossSalary, payePercentage, prsiPercentage, annualBonus, cycleToWorkMonthlyDeduction)
+}
